bfd3d38c7f23e17f199a0bdfa3cd40167dfa2c1f
YukiJiao <jiaotian0223@126.com>
2020-06-15 15:06:03 +0800

    Update daytime.py

--- daytime.py
+++ daytime.py
@@ -4,7 +4,7 @@ import datetime
 
 def time_day(dateTime):
     daylist = dateTime.split()
-    weekday = int(datetime.datetime.strptime(daylist[0],'%Y/%m/%d').strftime("%w"))
+    weekday = (int(datetime.datetime.strptime(daylist[0],'%Y/%m/%d').strftime("%w"))+1)/7
     time = (int(daylist[1].split(":")[0])*3600 + int(daylist[1].split(":")[1])*60 + int(daylist[1].split(":")[2]))/86400
     return daylist[0], weekday, time  
 
@@ -20,7 +20,29 @@ def gps_str_to_list(gps_data):
                        float(gps_list[i][1]),
                        round(float(gps_list[i][2])*3.6, 1),
                        day,  # 日期
-                       weekday,  # 周日-周六：0-6
+                       weekday,  # 周日-周六：1/7-7/7
                        time] # 归一化的当日时间
     return gps_list
+
+
+def tti_time_transfer(dateTime):
+    daylist = dateTime.split()
+    weekday = (int(datetime.datetime.strptime(daylist[0],'%Y-%m-%d').strftime("%w"))+1)/7
+    time = (int(daylist[1].split(":")[0])*3600 + int(daylist[1].split(":")[1])*60)/86400
+    return daylist[0], weekday, time  
+
+
+def preprocess_tti(tti_data):
+    tti_data['id_road'] = tti_data['id_road'].astype(str)
+    tti_data['TTI'] = tti_data['TTI'].astype(float)
+    tti_data['speed'] = tti_data['speed'].astype(float)
+    tti_data['time'] = pd.to_datetime(
+        tti_data['time'], infer_datetime_format=True)
+    tti_data['day'] = ''  # 日期
+    tti_data['weekday'] = ''  # 周日-周六：1/7-7/7
+    tti_data['daytime'] = ''  # 归一化的当日时间
+    for i in range(len(tti_data['time'])):
+        tti_data['day'][i], tti_data['weekday'][i], tti_data['daytime'][i] = tti_time_transfer(str(tti_data['time'][i]))
+    tti_data = tti_data.drop(columns = 'time')
+    return tti_data
 
