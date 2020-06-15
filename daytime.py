9234dcadde8f128f1e6a3b8c11e76b30654197ad
YukiJiao <jiaotian0223@126.com>
2020-06-15 16:21:26 +0800

    Update daytime.py

--- daytime.py
+++ daytime.py
@@ -13,12 +13,13 @@ def gps_str_to_list(gps_data):
     gps_list = gps_data[1:-1].split(',')
     gps_list = [i.split() for i in gps_list]
     for i in range(len(gps_list)):
-        # 去除方向数据, 切分时间数据
+        # 切分时间数据
         day, weekday, time = gps_time_transfer(pd.to_datetime(gps_list[i][4], unit='s', utc=True).tz_convert(
             "Asia/Shanghai").to_period("S").strftime('%Y/%m/%d %H:%M:%S'))
         gps_list[i] = [float(gps_list[i][0]),
                        float(gps_list[i][1]),
                        round(float(gps_list[i][2])*3.6, 1),
+                       float(gps_list[i][3]),
                        day,  # 日期
                        weekday,  # 周日-周六：1/7-7/7
                        time] # 归一化的当日时间
